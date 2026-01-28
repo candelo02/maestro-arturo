944a5366d7dcc010cf88863c8816a50fc6f6d553
candelo02 <candeloj2002@gmail.com>
2026-01-27 22:34:56 -0500

    actulizacion numero de telefono

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
 // Construye un enlace de WhatsApp con mensaje predefinido
-export function buildWhatsAppLink(message, phone = '5218123267811') {
+export function buildWhatsAppLink(message, phone = '525531987744') {
   try {
     return `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   } catch {
